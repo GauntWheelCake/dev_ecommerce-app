fe600b0e0dde4dfa8552a9987c4df77ba709cf79
GauntWheelCake <3268356371@qq.com>
2025-03-03 14:00:55 +0100

    Add Stripe payment gateway

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import adminAuth from '../middleware/adminAuth.js'
-import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus } from '../controllers/orderController.js'
+import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe } from '../controllers/orderController.js'
 import authUser from '../middleware/auth.js'
 
 const orderRouter = express.Router()
@@ -16,5 +16,8 @@ orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
 
 // User Feature
 orderRouter.post('/userorders', authUser, userOrders)
+
+// verify payment
+orderRouter.post('/verifyStripe', authUser, verifyStripe)
 
 export default orderRouter
