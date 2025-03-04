6c53b55fc868a46079353cc50cf28028bcaca92b
GauntWheelCake <3268356371@qq.com>
2025-03-04 22:18:21 +0100

    Home Page Modification and Give 30 Sample data

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,23 +3,23 @@ import { assets } from '../assets/assets'
 
 const Hero = () => {
     return (
-        <div className='flex flex-col sm:flex-row border border-gray-400'>
-            {/* {Hero Left Side} */}
-            <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
-                <div className='text-[#414141]'>
-                    <div className='flex items-center gap-2'>
+        <div className='flex flex-col sm:flex-row border border-gray-300'>
+            {/* Hero Left Side */}
+            <div className='w-3/5 sm:w-1/2 flex items-center justify-center py-5 sm:py-2'>
+                <div className='text-[#414141] text-center sm:text-left'>
+                    <div className='flex items-center gap-2 justify-center sm:justify-start'>
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                        <p className=' font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+                        <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
                     </div>
-                    <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
-                    <div className='flex items-center gap-2'>
+                    <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Furniture</h1>
+                    <div className='flex items-center gap-2 justify-center sm:justify-start'>
                         <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                         <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
                     </div>
                 </div>
             </div>
-            {/* Hero Rigth Side */}
-            <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
+            {/* Hero Right Side */}
+            <img className='w-2/5 sm:w-2/5' src={assets.hero_img1} alt="" />
         </div>
     )
 }
