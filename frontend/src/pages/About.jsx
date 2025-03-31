f6b2a4b779d70fc169adb768f406ad43c36b879e
GauntWheelCake <3268356371@qq.com>
2025-03-31 10:58:07 +0200

    nothing important

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <div className='text-2xl text-center pt-8 border-t'>
                 <Title text1={'ABOUT'} text2={'US'} />
             </div>
-            <div className='my-10 flex flex-col md:flex-row gap-16'>
+            <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
                 <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
                 <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
                     <p>Our goal is to develop an IKEA-like e-commerce platform that allows users to browse, purchase, and customize  home furniture and decorations. The platform will provide a seamless shopping experience with intuitive navigation, a rich product catalog, and personalized recommendations . Key functionalities include user authentication, product management, shopping carts, secure payment processing, and order tracking.</p>
